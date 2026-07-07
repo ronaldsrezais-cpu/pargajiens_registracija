47b7d63bdacd4aab60ef9e09fabb69ddb4c86d92
ronaldsrezais-cpu <ronalds.rezais@gmail.com>
2026-07-08 00:16:26 +0300

    Add files via upload

--- app/settings.ts
+++ app/settings.ts
@@ -1,3 +1,3 @@
 // Paste the Google Apps Script Web App URL here, the same practical setup style as the Home & Heart website.
 // Use the deployed URL ending with /exec.
-export const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx66D4TYrb-Hs96Jg9ejmvFFdcoEbb2b4H1PZ0M6OqZgGyfDjC11hxJElv76xwwd0Ml_A/exec';
+export const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyLh4C3gZgiujyjO6TTQK-19EjtkowF5Xf4Yx5z5mM6AKC0_2XltHLXKGoTGdIDwLIXQQ/exec';
